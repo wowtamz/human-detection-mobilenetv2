fafcf257258af2ad592a9c5d4489c494dd3aa55a
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-04-29 21:04:04 +0200

    added and updated a1 files

--- mmp/a1/main.py
+++ mmp/a1/main.py
@@ -1,6 +1,9 @@
 from typing import Sequence
+from PIL import Image
+import json
 import torch
-
+import torchvision
+import numpy as np
 
 def build_batch(paths: Sequence[str], transform=None) -> torch.Tensor:
     """Exercise 1.1
@@ -9,7 +12,22 @@ def build_batch(paths: Sequence[str], transform=None) -> torch.Tensor:
     @param transform: One or multiple image transformations for augmenting the batch images.
     @return: Returns one single tensor that contains every image.
     """
-    raise NotImplementedError()
+
+    images = list()
+
+    tfm = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        torchvision.transforms.Resize((224, 224))
+    ])
+
+    for path in paths:
+        img = Image.open(path)
+        img_tensor = transform(img) if transform else tfm(img)
+        images.append(img_tensor)
+
+    img_batch = torch.stack(images)
+    return img_batch
 
 
 def get_model() -> torch.nn.Module:
@@ -17,7 +35,10 @@ def get_model() -> torch.nn.Module:
 
     @return: Returns a neural network, initialised with pretrained weights.
     """
-    raise NotImplementedError()
+    model = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.IMAGENET1K_V1)
+    model.eval()
+    
+    return model
 
 
 def main():
@@ -25,8 +46,74 @@ def main():
 
     Put all your code for exercise 1.3 here.
     """
-    raise NotImplementedError()
 
+    img_paths = [
+        "images/golden retriever.jpg",
+        "images/koala.jpg",
+        "images/pacifier.jpg",
+        "images/rubber ducks.jpg",
+        "images/rubber duck sculpture.jpg",
+        "images/shoehorn.jpg",
+        "images/zoo.jpg",
+    ]
+
+    # (a)
+    def_img_batch = build_batch(img_paths)
+
+    # (b)
+    tfm_b = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        torchvision.transforms.Resize((512, 512))
+    ])
+    resized_img_batch = build_batch(img_paths, tfm_b)
+
+    # (c)
+    tfm_c = torchvision.transforms.Compose([
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        torchvision.transforms.Resize((224, 224)),
+        torchvision.transforms.RandomVerticalFlip(1.0)
+    ])
+    vertically_flipped_img_batch = build_batch(img_paths, tfm_c)
+
+    model = get_model()
+
+    with open("imagenet_classes.json") as f:
+        classmap = json.load(f)
+
+    print(8*"*")
+    print("Default transforms")
+    print(8*"*")
+
+    output = model(def_img_batch)
+    top_pred = output.argmax(dim=1)
+
+    for i in range(len(img_paths)):
+        expecting = img_paths[i].removeprefix("images/").removesuffix(".jpg").capitalize()
+        print(f"Prediction for {expecting}: {classmap[top_pred[i]]}")
+    
+    print(8*"*")
+    print("Resized images to 512 x 512")
+    print(8*"*")
+
+    output = model(resized_img_batch)
+    top_pred = output.argmax(dim=1)
+
+    for i in range(len(img_paths)):
+        expecting = img_paths[i].removeprefix("images/").removesuffix(".jpg").capitalize()
+        print(f"Prediction for {expecting}: {classmap[top_pred[i]]}")
+
+    print(8*"*")
+    print("Flipped images vertically")
+    print(8*"*")
+
+    output = model(vertically_flipped_img_batch)
+    top_pred = output.argmax(dim=1)
+
+    for i in range(len(img_paths)):
+        expecting = img_paths[i].removeprefix("images/").removesuffix(".jpg").capitalize()
+        print(f"Prediction for {expecting}: {classmap[top_pred[i]]}")
 
 if __name__ == "__main__":
     main()
--- mmp/a1/tensors.py
+++ mmp/a1/tensors.py
@@ -1,10 +1,19 @@
 import torch
+import torchvision
+from PIL import Image
 
 def avg_color(img: torch.Tensor):
-    raise NotImplementedError()
+    #r = img[0]
+    #g = img[1]
+    #b = img[2]
+    return img.mean(dim=1).mean(dim=1) # torch.tensor([r.mean(), g.mean(), b.mean()])
 
 def mask(foreground: torch.Tensor, background: torch.Tensor, mask_tensor: torch.Tensor, threshold: float):
-    raise NotImplementedError()
+    
+    condition = mask_tensor > threshold
+    background[condition] = foreground
+    return background
 
 def add_matrix_vector(matrix: torch.Tensor, vector: torch.Tensor):
-    raise NotImplementedError()
+    mat = matrix.T + vector
+    return mat
