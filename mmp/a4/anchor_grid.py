42e53d007401a968addd9992ef5a5b8a3d804492
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-05-20 10:40:01 +0200

    implemented anchor grid

--- mmp/a4/anchor_grid.py
+++ mmp/a4/anchor_grid.py
@@ -9,4 +9,20 @@ def get_anchor_grid(
     anchor_widths: Sequence[float],
     aspect_ratios: Sequence[float],
 ) -> np.ndarray:
-    raise NotImplementedError()
+    
+    grid = np.ndarray((num_rows, num_cols, anchor_widths, aspect_ratios))
+
+    for i in range(num_rows):
+        for j in range(num_cols):
+            for w in range(len(anchor_widths)):
+                for r in range(len(aspect_ratios)):
+                    # aspect ration = height / width => height = aspect_ration * width
+                    h = r * w
+                    box_center_x = (j * scale_factor) + (scale_factor / 2)
+                    box_center_y = (i * scale_factor) + (scale_factor / 2)
+                    box_x1 = box_center_x - (w / 2)
+                    box_y1 = box_center_y - (h / 2)
+                    box_x2 = box_center_x + (w / 2)
+                    box_y2 = box_center_y + (h / 2)
+
+                    grid[i][j][w][r] = np.array([box_x1, box_y1, box_x2, box_y2])
