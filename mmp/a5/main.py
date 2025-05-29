fb4b64e253dc360b394f997a455bffc998d6ed7e
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-05-29 10:43:54 +0200

    implemented main for assignment 5

--- mmp/a5/main.py
+++ mmp/a5/main.py
@@ -1,8 +1,15 @@
 import torch
 import torch.optim as optim
-
+import torchvision
+from PIL import Image
 from .model import MmpNet
+from ..a4 import dataset
+from ..a4 import anchor_grid
+from . import model as md
+from ..a2 import main as a2
 
+curr_epoch = 0
+curr_batch = 0
 
 def step(
     model: MmpNet,
@@ -29,11 +36,61 @@ def get_random_sampling_mask(labels: torch.Tensor, neg_ratio: float) -> torch.Te
     assert labels.min() >= 0 and labels.max() <= 1  # remove this line if you want
     raise NotImplementedError()
 
+def train_epoch(model, loader: dataset.DataLoader, criterion, optimizer, device):
+    
+    global curr_batch
+    global curr_epoch
+    
+    model = model.to(device)
+    model.train()
+
+    for batch in loader:
+
+        images, labels, ids = batch
+
+        images = images.to(device)
+        labels = labels.to(device)
+
+        loss = md.step(model, criterion, optimizer, images, labels.float())
+        print(f"e:{curr_epoch}/b:{curr_batch}/l:{loss}")
+        curr_batch += 1
 
 def main():
     """Put your training code for exercises 5.2 and 5.3 here"""
-    raise NotImplementedError()
 
+    data_path = "dataset/train"
+    img_size = 224
+    batch_size = 8
+    num_workers = 0
+
+    scale_factor = 4.0
+
+    num_rows = int(img_size / scale_factor)
+    num_cols = int(img_size / scale_factor)
+
+    anchor_widths = [2, 4, 8, 16, 32, 64, 128, 256]
+    aspect_ratios = [0.1, 0.25, 0.5, 0.75, 1.0, 1.5, 2, 3]
+
+    agrid = anchor_grid.get_anchor_grid(
+        num_rows,
+        num_cols, 
+        scale_factor,
+        anchor_widths,
+        aspect_ratios
+    )
+
+    model = MmpNet(len(anchor_widths) ,len(aspect_ratios))
+    model.set_classifier_head(num_rows, num_cols)
+    dataloader = dataset.get_dataloader(data_path, img_size, batch_size, num_workers, agrid, False)
+
+    loss_func, optimizer = a2.get_criterion_optimizer(model)
+
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+    epochs = 10
+    for i in range(epochs):
+        curr_epoch = i
+        train_epoch(model, dataloader, loss_func, optimizer, device)
 
 if __name__ == "__main__":
     main()
