1585983786a4b660936cb52a378a62f2ae3cbf3f
Tamas Varadi <varadita@mmc117.informatik.uni-augsburg.de>
2025-06-03 10:42:50 +0200

    using loops to dynamically test settings

--- mmp/a5/main.py
+++ mmp/a5/main.py
@@ -194,47 +194,17 @@ def main():
     """Put your training code for exercises 5.2 and 5.3 here"""
     
     _tag = "_testing"
-    _epochs = 15
-    train_and_evaluate(
-        scale_factor=8.0,
-        anchor_widths=[8, 16, 32, 64, 128],
-        aspect_ratios=[0.5, 1.0, 1.5, 2.0],
-        tag=_tag, neg_mining=False, epochs=_epochs)
-    
-    train_and_evaluate(
-        scale_factor=4.0,
-        anchor_widths=[8, 16, 32, 64, 128],
-        aspect_ratios=[0.5, 1.0, 1.5, 2.0],
-        tag=_tag, neg_mining=True, epochs=_epochs)
-    
-    train_and_evaluate(
-        scale_factor=8.0,
-        learn_rate=0.004,
-        anchor_widths=[8, 16, 32, 64, 128],
-        aspect_ratios=[0.5, 1.0, 1.5, 2.0],
-        tag=_tag, neg_mining=False, epochs=_epochs)
-    
-    train_and_evaluate(
-        scale_factor=4.0,
-        learn_rate=0.004,
-        anchor_widths=[8, 16, 32, 64, 128],
-        aspect_ratios=[0.5, 1.0, 1.5, 2.0],
-        tag=_tag, neg_mining=True, epochs=_epochs)
-    
-    train_and_evaluate(
-        scale_factor=8.0,
-        learn_rate=0.008,
-        anchor_widths=[8, 16, 32, 64, 128],
-        aspect_ratios=[0.5, 1.0, 1.5, 2.0],
-        tag=_tag, neg_mining=False, epochs=_epochs)
-    
-    train_and_evaluate(
-        scale_factor=4.0,
-        learn_rate=0.008,
-        anchor_widths=[8, 16, 32, 64, 128],
-        aspect_ratios=[0.5, 1.0, 1.5, 2.0],
-        tag=_tag, neg_mining=True, epochs=_epochs)
+    _epochs = 100
 
-
+    for sf in [4.0, 0.6, 0.8]:
+        for lr in [0.004, 0.008, 0.01, 0.015, 0.02]:
+            for neg_mine in [False, True]:
+                train_and_evaluate(
+                scale_factor=sf,
+                learn_rate=lr,
+                anchor_widths=[8, 16, 32, 64, 128],
+                aspect_ratios=[0.5, 1.0, 1.5, 2.0],
+                tag=_tag, neg_mining=neg_mine, epochs=_epochs)
+    
 if __name__ == "__main__":
     main()
