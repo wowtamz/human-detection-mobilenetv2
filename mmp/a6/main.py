87405f57caf8eb5c1c3fead41348ab97e6686019
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-06-17 12:40:16 +0200

    implemented evaluate() in a6/main.py

--- mmp/a6/main.py
+++ mmp/a6/main.py
@@ -5,13 +5,21 @@ import numpy as np
 from ..a3.annotation import AnnotationRect
 from ..a4.label_grid import iou
 from ..a5.model import MmpNet
+from ..a5.main import get_criterion_optimizer
 from .nms import non_maximum_suppression
+
+# Only import tensorboard if it is installed
+try:
+    from torch.utils.tensorboard import SummaryWriter
+except ImportError:
+    SummaryWriter = None
 
 def batch_inference(
     model: MmpNet, images: torch.Tensor, device: torch.device, anchor_grid: np.ndarray
 ) -> List[List[Tuple[AnnotationRect, float]]]:
-    
+
     result = []
+    images = images.to(device)
 
     for i in images:
         boxes_scores = []
@@ -33,15 +41,42 @@ def batch_inference(
 
     return result
 
-def evaluate() -> float:  # feel free to change the arguments
+def evaluate(model, loader, device, tensorboard_writer, anchor_grid) -> float:  # feel free to change the arguments
     """Evaluates a specified model on the whole validation dataset.
 
     @return: AP for the validation set as a float.
 
     You decide which arguments this function should receive
     """
-    raise NotImplementedError()
+    
+    model = model.to(device)
+    model.eval()
 
+    precisions = np.array()
+    recalls = np.array()
+
+    with torch.no_grad():
+        for i, data in enumerate(loader):
+            images, labels, ids = data
+            
+            inference = batch_inference(model, images, device, anchor_grid)
+            tp = torch.sum(inference == labels)
+            fp = torch.sum(inference == True and labels == False)
+            fn = torch.sum(inference == False and labels == True)
+
+            prec = tp / (tp + fp)
+            rec = tp / (tp + fn)
+            precisions.append(prec)
+            recalls.append(rec)
+            print(f"Evaluating batch {i}, Precision ({prec}), Recall ({rec})")
+
+    avg_precision = np.average(precisions)
+    avg_recall = np.average(recalls)
+
+    if tensorboard_writer:
+        tensorboard_writer.add_scalar("Precision/Recall", avg_precision, avg_recall)
+    
+    return avg_precision
 
 def evaluate_test():  # feel free to change the arguments
     """Generates predictions on the provided test dataset.
