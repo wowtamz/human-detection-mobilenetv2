23523bc1ce8bffefabd27252e6215b334dbd9f61
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-06-17 11:57:10 +0200

    implemented batch_inference() in a6/main.py

--- mmp/a6/main.py
+++ mmp/a6/main.py
@@ -2,15 +2,36 @@ from typing import List, Tuple
 import torch
 import numpy as np
 
-from ..a5.model import MmpNet
 from ..a3.annotation import AnnotationRect
-
+from ..a4.label_grid import iou
+from ..a5.model import MmpNet
+from .nms import non_maximum_suppression
 
 def batch_inference(
     model: MmpNet, images: torch.Tensor, device: torch.device, anchor_grid: np.ndarray
 ) -> List[List[Tuple[AnnotationRect, float]]]:
-    raise NotImplementedError()
+    
+    result = []
 
+    for i in images:
+        boxes_scores = []
+        predicition = model(i)
+        widths, ratios, rows, cols = predicition.shape
+        for w in range(widths):
+            for a in range(ratios):
+                for r in range(rows):
+                    for c in range(cols):
+                        pred_box_array = predicition[w][a][r][c]
+                        pred_rect = AnnotationRect.fromarray(pred_box_array)
+
+                        anchor_box_array = anchor_grid[w][a][r][c]
+                        anchor_rect = AnnotationRect.fromarray(anchor_box_array)
+                        score = iou(pred_rect, anchor_rect)
+                        boxes_scores.append((pred_rect, score))
+        filtered = non_maximum_suppression(boxes_scores)
+        result.append(filtered)
+
+    return result
 
 def evaluate() -> float:  # feel free to change the arguments
     """Evaluates a specified model on the whole validation dataset.
