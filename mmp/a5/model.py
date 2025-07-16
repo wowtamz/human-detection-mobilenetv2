d10d32fd628805eed5dd7b6139e993141e73b7e2
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-07-16 21:00:29 +0200

    added Dropout to classification layer of model; added bbr layer to model

--- mmp/a5/model.py
+++ mmp/a5/model.py
@@ -3,12 +3,13 @@ import torch.nn as nn
 import torchvision
 
 class MmpNet(torch.nn.Module):
-    def __init__(self, num_widths: int, num_aspect_ratios: int, rows: int = 28, cols: int = 28):
+    def __init__(self, num_widths: int, num_aspect_ratios: int, rows: int = 28, cols: int = 28, use_bbr = False):
         super().__init__()
         self.num_widths = num_widths
         self.num_aspect_ratios = num_aspect_ratios
         self.rows = rows
         self.cols = cols
+        self.use_bbr = use_bbr
         self.model = torchvision.models.mobilenet_v2(weights = "DEFAULT").features # only use mobilenet's features (ex. 5.1 b)
 
         # custom Classifier Label Grid 4 (Page 14)
@@ -17,7 +18,14 @@ class MmpNet(torch.nn.Module):
         
         self.custom = nn.Sequential(
             nn.Conv2d(channels_in, channels_out, kernel_size=1),
+            nn.Dropout2d(p=0.2),
             nn.Upsample(size=(rows, cols), mode="bilinear", align_corners=False) # Upsample to match (rows, cols)
+        )
+
+        self.bbr_layers = nn.Sequential(
+            nn.Conv2d(channels_in, 4 * num_widths * num_aspect_ratios, kernel_size=1),
+            nn.Dropout2d(p=0.2),
+            nn.Upsample(size=(rows, cols), mode="bilinear", align_corners=False)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -26,5 +34,12 @@ class MmpNet(torch.nn.Module):
         # resize 1 dim tensor to (batch_size, 2, width_sizes, ratios_sizes, rows, cols)
         # shape is not the same due to batching
         batch_size = x.shape[0]
-        out =  out.view(batch_size, 2, self.num_widths, self.num_aspect_ratios, self.rows, self.cols)
-        return out
+        anchor_output =  out.view(batch_size, 2, self.num_widths, self.num_aspect_ratios, self.rows, self.cols)
+        
+        if self.use_bbr:
+            bbr_output = self.bbr_layers(features)
+            bbr_output = bbr_output.view(batch_size, 4, self.num_widths, self.num_aspect_ratios, self.rows, self.cols)
+        else:
+            bbr_output = None
+
+        return anchor_output, bbr_output
