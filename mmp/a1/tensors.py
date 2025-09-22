760d73535b81d2a3caa73292ef86d2d2d4a3e119
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-09-22 17:14:50 +0200

    revert overwrite

--- mmp/a1/tensors.py
+++ mmp/a1/tensors.py
@@ -1,10 +1,24 @@
 import torch
+import torchvision
+from PIL import Image
 
 def avg_color(img: torch.Tensor):
-    raise NotImplementedError()
+    #r = img[0]
+    #g = img[1]
+    #b = img[2]
+    return img.mean(dim=1).mean(dim=1) # torch.tensor([r.mean(), g.mean(), b.mean()])
 
 def mask(foreground: torch.Tensor, background: torch.Tensor, mask_tensor: torch.Tensor, threshold: float):
-    raise NotImplementedError()
+    
+    if mask_tensor.dim() == 2:
+        mask_tensor = mask_tensor.unsqueeze(0)
+    
+    mask_expanded = mask_tensor.expand_as(foreground)
+
+    condition = mask_expanded > threshold
+    background[condition] = foreground[condition]
+    return background
 
 def add_matrix_vector(matrix: torch.Tensor, vector: torch.Tensor):
-    raise NotImplementedError()
+    mat = matrix + vector.unsqueeze(1)
+    return mat
