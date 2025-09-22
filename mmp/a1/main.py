0ba1744ef275d5390dd28cf4511041352c9c4b23
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-09-22 17:14:34 +0200

    update template code

--- mmp/a1/main.py
+++ mmp/a1/main.py
@@ -1,9 +1,6 @@
 from typing import Sequence
-from PIL import Image
-import numpy as np
-import json
 import torch
-import torchvision
+
 
 def build_batch(paths: Sequence[str], transform=None) -> torch.Tensor:
     """Exercise 1.1
@@ -12,30 +9,7 @@ def build_batch(paths: Sequence[str], transform=None) -> torch.Tensor:
     @param transform: One or multiple image transformations for augmenting the batch images.
     @return: Returns one single tensor that contains every image.
     """
-
-    images = list()
-    transforms = list()
-    
-    if transform:
-        transforms.append(transform)
-    
-    if not isinstance(transform, torchvision.transforms.Resize):
-        transforms.append(torchvision.transforms.Resize((224, 224)))
-
-    transforms.append(torchvision.transforms.ToTensor())
-    transforms.append(torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]))
-
-    tfm = torchvision.transforms.Compose(transforms)
-
-    for path in paths:
-        img = Image.open(path)
-        img_tensor = tfm(img)
-        if not isinstance(img_tensor, torch.Tensor):
-            img_tensor = torch.tensor(img_tensor)
-        images.append(img_tensor)
-
-    img_batch = torch.stack(images)
-    return img_batch
+    raise NotImplementedError()
 
 
 def get_model() -> torch.nn.Module:
@@ -43,57 +17,16 @@ def get_model() -> torch.nn.Module:
 
     @return: Returns a neural network, initialised with pretrained weights.
     """
-    model = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.IMAGENET1K_V1)
-    model.eval()
-    
-    return model
+    raise NotImplementedError()
 
-def print_predictions(title, model, img_batch, img_paths, classmap):
-    
-    print(8*"*")
-    print(f"{title}")
-    print(8*"*")
-
-    output = model(img_batch)
-    top_pred = output.argmax(dim=1)
-
-    for i in range(len(img_paths)):
-        expecting = img_paths[i].removeprefix("images/").removesuffix(".jpg").capitalize()
-        print(f"Prediction for {expecting}: {classmap[top_pred[i]]}")
 
 def main():
     """Exercise 1.3
 
     Put all your code for exercise 1.3 here.
     """
+    raise NotImplementedError()
 
-    img_paths = [
-        "images/golden retriever.jpg",
-        "images/koala.jpg",
-        "images/pacifier.jpg",
-        "images/rubber ducks.jpg",
-        "images/rubber duck sculpture.jpg",
-        "images/shoehorn.jpg",
-        "images/zoo.jpg",
-    ]
-
-    # (a)
-    def_img_batch = build_batch(img_paths)
-
-    # (b)
-    resized_img_batch = build_batch(img_paths, torchvision.transforms.Resize((512, 512)))
-
-    # (c)
-    vertically_flipped_img_batch = build_batch(img_paths, torchvision.transforms.RandomVerticalFlip(1.0))
-
-    model = get_model()
-
-    with open("imagenet_classes.json") as f:
-        classmap = json.load(f)
-
-    print_predictions("Default Transforms", model, def_img_batch, img_paths, classmap)
-    print_predictions("Resized Images to 512 x 512", model, resized_img_batch, img_paths, classmap)
-    print_predictions("Flipped Images Vertically", model, vertically_flipped_img_batch, img_paths, classmap)
 
 if __name__ == "__main__":
     main()
--- mmp/a1/tensors.py
+++ mmp/a1/tensors.py
@@ -1,24 +1,10 @@
 import torch
-import torchvision
-from PIL import Image
 
 def avg_color(img: torch.Tensor):
-    #r = img[0]
-    #g = img[1]
-    #b = img[2]
-    return img.mean(dim=1).mean(dim=1) # torch.tensor([r.mean(), g.mean(), b.mean()])
+    raise NotImplementedError()
 
 def mask(foreground: torch.Tensor, background: torch.Tensor, mask_tensor: torch.Tensor, threshold: float):
-    
-    if mask_tensor.dim() == 2:
-        mask_tensor = mask_tensor.unsqueeze(0)
-    
-    mask_expanded = mask_tensor.expand_as(foreground)
-
-    condition = mask_expanded > threshold
-    background[condition] = foreground[condition]
-    return background
+    raise NotImplementedError()
 
 def add_matrix_vector(matrix: torch.Tensor, vector: torch.Tensor):
-    mat = matrix + vector.unsqueeze(1)
-    return mat
+    raise NotImplementedError()
