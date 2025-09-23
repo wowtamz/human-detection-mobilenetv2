486e40c4e7166d4eb53b5f77b988daf04a2be384
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-09-23 14:30:29 +0200

    created annotation.py copy in utils directory

---
+++ utils/annotation.py
@@ -0,0 +1,118 @@
+from typing import List
+import numpy as np
+import os
+import math
+from PIL import Image, ImageDraw
+
+class AnnotationRect:
+
+    def __init__(self, x1, y1, x2, y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+
+    def area(self):
+        height = abs(self.y1 - self.y2)
+        width = abs(self.x1 - self.x2)
+        return height * width
+    
+    def scaled(self, scale):
+        return AnnotationRect(
+            self.x1 * scale,
+            self.y1 * scale,
+            self.x2 * scale,
+            self.y2 * scale
+        )
+    
+    def rescaled(self, scale):
+        return AnnotationRect(
+            self.x1 / scale,
+            self.y1 / scale,
+            self.x2 / scale,
+            self.y2 / scale
+        )
+    
+    def flip_horizontal(self, img_size):
+        new_x1 = img_size - self.x2
+        new_x2 = img_size - self.x1
+        self.x1, self.x2 = new_x1, new_x2
+    
+    def rotate(self, degrees, img_size):
+        degrees = -degrees # Degrees need to be inverted
+        angle = math.radians(degrees)
+        cx, cy = img_size / 2, img_size / 2
+
+        points = np.array([
+            [self.x1, self.y1],
+            [self.x2, self.y1],
+            [self.x2, self.y2],
+            [self.x1, self.y2]
+        ])
+
+        rot_matrix = np.array([
+            [math.cos(angle), -math.sin(angle)],
+            [math.sin(angle), math.cos(angle)]
+        ])
+
+        translated = points - np.array([cx, cy])
+        rotated = np.dot(translated, rot_matrix.T)
+        rotated += np.array([cx, cy])
+
+        x_coords = rotated[:, 0]
+        y_coords = rotated[:, 1]
+
+        return AnnotationRect(
+            x_coords.min(),
+            y_coords.min(),
+            x_coords.max(),
+            y_coords.max()
+        )
+
+    def __array__(self) -> np.ndarray:
+        return np.array([self.x1, self.y1, self.x2, self.y2], dtype=np.int64)
+
+    @staticmethod
+    def fromarray(arr: np.ndarray):
+        return AnnotationRect(arr[0], arr[1], arr[2], arr[3])
+
+def read_groundtruth_file(path: str) -> List[AnnotationRect]:
+
+    annotationRects = list()
+    with open(path) as f:
+        for line in f.readlines():
+            tokens = line.split(" ")
+            values = np.array(list(float(t) for t in tokens))
+            annotationRects.append(AnnotationRect.fromarray(values))
+    return annotationRects
+
+def draw_img_with_max_annotation_count(dir_path):
+    img_path = get_img_with_max_annotation_count(dir_path)
+    draw_img(img_path)
+
+def get_img_with_max_annotation_count(path: str) -> str:
+    groundtruthFiles = list(gt for gt in os.listdir(path) if gt.endswith("gt_data.txt"))
+    result = (None, 0)
+    for gt_filename in groundtruthFiles:
+        gt_file_path = path + gt_filename if path.endswith("/") else path + "/" + gt_filename
+        annotations = read_groundtruth_file(gt_file_path)
+        annotionCount = len(annotations)
+        if result[1] < annotionCount:
+            result = (gt_file_path.replace(".gt_data.txt", ".jpg"), annotionCount)
+    return result[0]
+
+def draw_annotation(img_draw: np.array, ann: np.array, color = "blue", thickness: int = 3):
+    img_draw.rectangle([(int(ann[0]), int(ann[1])), (int(ann[2]), int(ann[3]))], outline=color, width=thickness)
+
+def draw_img(path: str):
+
+    img = Image.open(path)
+
+    groundtruthPath = path.replace(".jpg", ".gt_data.txt")
+    annotations = read_groundtruth_file(groundtruthPath)
+
+    for annotation in annotations:
+        img_draw = ImageDraw.Draw(img)
+        draw_annotation(img_draw, annotation.__array__())
+
+    img.show()
