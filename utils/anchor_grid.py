036de210a1d6f3d0c928d1eca9deffac2f9c1e3b
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-09-23 14:39:21 +0200

    create anchor_grid and label_grid copies in utils/ direcatory

---
+++ utils/anchor_grid.py
@@ -0,0 +1,31 @@
+from typing import Sequence
+import numpy as np
+
+def get_anchor_grid(
+    num_rows: int,
+    num_cols: int,
+    scale_factor: float,
+    anchor_widths: Sequence[float],
+    aspect_ratios: Sequence[float],
+) -> np.ndarray:
+    
+    grid = np.zeros((len(anchor_widths), len(aspect_ratios), num_rows, num_cols, 4))
+
+    for row in range(num_rows):
+        for col in range(num_cols):
+            center_x = (col + 0.5) * scale_factor
+            center_y = (row + 0.5) * scale_factor
+            for width_idx in range(len(anchor_widths)):
+                for ratio_idx in range(len(aspect_ratios)):
+                    # aspect ration = height / width => height = aspect_ration * width
+                    width = anchor_widths[width_idx]
+                    ratio = aspect_ratios[ratio_idx]
+                    height = ratio * width
+                    
+                    box_x1 = max(0.0, center_x - (width / 2))
+                    box_y1 = max(0.0, center_y - (height / 2))
+                    box_x2 = min(224.0, center_x + (width / 2))
+                    box_y2 = min(224.0, center_y + (height / 2))
+
+                    grid[width_idx, ratio_idx, row, col] = [box_x1, box_y1, box_x2, box_y2]
+    return grid
---
+++ utils/label_grid.py
@@ -0,0 +1,71 @@
+from typing import Sequence
+from utils.annotation import AnnotationRect
+import numpy as np
+
+def iou(rect1: AnnotationRect, rect2: AnnotationRect) -> float:
+    intersect_rect_x1 = max(rect1.x1, rect2.x1)
+    intersect_rect_y1 = max(rect1.y1, rect2.y1)
+    intersect_rect_x2 = min(rect1.x2, rect2.x2)
+    intersect_rect_y2 = min(rect1.y2, rect2.y2)
+    
+    if (intersect_rect_x1 >= intersect_rect_x2 or intersect_rect_y1 >= intersect_rect_y2):
+        return 0.0
+
+    union_rect_x1 = min(rect1.x1, rect2.x1)
+    union_rect_y1 = min(rect1.y1, rect2.y1)
+    union_rect_x2 = max(rect1.x2, rect2.x2)
+    union_rect_y2 = max(rect1.y2, rect2.y2)
+
+    intersect_rect = AnnotationRect(intersect_rect_x1, intersect_rect_y1, intersect_rect_x2, intersect_rect_y2)
+    union_rect = AnnotationRect(union_rect_x1, union_rect_y1, union_rect_x2, union_rect_y2)
+    union_area = rect1.area() + rect2.area() - intersect_rect.area()
+
+    return intersect_rect.area() / union_area
+
+
+def get_label_grid(
+    anchor_grid: np.ndarray, gts: Sequence[AnnotationRect], min_iou: float
+) -> tuple[np.ndarray, ...]:
+    
+    sizes, ratios, rows, cols, points = anchor_grid.shape
+
+    grid_size = sizes * ratios * rows * cols
+
+    grid = np.zeros(grid_size, dtype=bool) # Preallocate flat grid memory
+
+    anchor_grid_flat = anchor_grid.reshape(-1, 4)
+    
+    for i in range(grid_size):
+        for gt in gts:
+            rect = anchor_grid_flat[i]
+            if iou(AnnotationRect.fromarray(rect), gt) >= min_iou:
+                grid[i] = True
+                continue
+    
+    grid = grid.reshape((sizes, ratios, rows, cols))
+
+    '''
+    grid = np.zeros((sizes, ratios, rows, cols), dtype=bool)
+
+    for size in range(sizes):
+        for ratio in range(ratios):
+            for row in range(rows):
+                for col in range(cols):
+                    for gt in gts:
+                        rect = anchor_grid[size, ratio, row, col]
+                        if iou(AnnotationRect.fromarray(rect), gt) >= min_iou:
+                            grid[size, ratio, row, col] = True
+    '''    
+    return grid
+
+def get_matching_rects(anch_grid, label_grid):
+    rects = []
+    sizes, ratios, rows, cols, _ = anch_grid.shape
+    for size in range(sizes):
+        for ratio in range(ratios):
+            for row in range(rows):
+                for col in range(cols):
+                    if label_grid[size, ratio, row, col]:
+                        rect = AnnotationRect.fromarray(anch_grid[size, ratio, row, col])
+                        rects.append(rect)
+    return rects
