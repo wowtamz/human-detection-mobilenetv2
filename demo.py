fb544f7db4647a2e32309ce880415c26e52ee669
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-09-24 16:46:17 +0200

    created demo.py and models/simplenet.py

---
+++ demo.py
@@ -0,0 +1,7 @@
+from models.simplenet import SimpleNet
+
+def main():
+    epochs = input("Please specify the amount of epochs to train")
+
+if __name__ == "__main__":
+    main()
---
+++ models/simplenet.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import torchvision
+
+class SimpleNet(torch.nn.Module):
+    def __init__(self, num_widths: int, num_aspect_ratios: int, rows: int = 28, cols: int = 28):
+        super().__init__()
+        self.num_widths = num_widths
+        self.num_aspect_ratios = num_aspect_ratios
+        self.rows = rows
+        self.cols = cols
+        
+        # only use mobilenet's features
+        self.model = torchvision.models.mobilenet_v2(weights = "DEFAULT").features
+
+        # custom Classifier
+        channels_in = self.model[-1][0].out_channels
+        channels_out = 2 * num_widths * num_aspect_ratios
+        
+        self.custom = nn.Sequential(
+            nn.Conv2d(channels_in, channels_out, kernel_size=1),
+            nn.Dropout2d(p=0.2),
+            nn.Upsample(size=(rows, cols), mode="bilinear", align_corners=False) # Upsample to match (rows, cols)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        features = self.model(x)
+        out = self.custom(features)
+        # resize 1 dim tensor to (batch_size, 2, width_sizes, ratios_sizes, rows, cols)
+        # shape is not the same due to batching
+        batch_size = x.shape[0]
+        anchor_output =  out.view(batch_size, 2, self.num_widths, self.num_aspect_ratios, self.rows, self.cols)
+
+        return anchor_output, None # bbr prediction placeholder
