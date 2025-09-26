2d9d81200d5202cfdd7d7f927f31154c27688c35
Tamas Varadi <1997tamas.varadi@gmail.com>
2025-09-26 20:46:56 +0200

    created child class BBRNet which implements bounding box regression

--- models/simplenet.py
+++ models/simplenet.py
@@ -32,3 +32,26 @@ class SimpleNet(torch.nn.Module):
         anchor_output =  out.view(batch_size, 2, self.num_widths, self.num_aspect_ratios, self.rows, self.cols)
 
         return anchor_output, None # bbr prediction placeholder
+
+class BBRNet(SimpleNet):
+    def __init__(self, num_widths: int, num_aspect_ratios: int, rows: int = 28, cols: int = 28):
+        super().__init__(num_widths, num_aspect_ratios, rows, cols)
+
+        channels_in = self.model[-1][0].out_channels
+
+        self.bbr_layers = nn.Sequential(
+            nn.Conv2d(channels_in, 4 * num_widths * num_aspect_ratios, kernel_size=1),
+            nn.Dropout2d(p=0.2),
+            nn.Upsample(size=(rows, cols), mode="bilinear", align_corners=False)
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        features = self.model(x)
+        batch_size = x.shape[0]
+        
+        bbr_output = self.bbr_layers(features)
+        bbr_output = bbr_output.view(batch_size, self.num_widths, self.num_aspect_ratios, self.rows, self.cols, 4)
+        
+        anchor_output = super().forward(x)
+
+        return anchor_output, bbr_output
